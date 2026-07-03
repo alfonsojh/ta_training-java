c886c95feb9086e0ea80ee0b6340b161bc636300
Alfonso Javier <alfonso_javier@nube.unadmexico.mx>
2026-07-03 12:26:11 -0700

    Adding Tasks

---
+++ src/main/java/com/epam/training/alfonso_javier/Fundamentals/Task1/HelloAutocode.java
@@ -0,0 +1,8 @@
+package com.epam.training.alfonso_javier.Fundamentals.Task1;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        System.out.println("Hello, Autocode!");
+
+    }
+}
